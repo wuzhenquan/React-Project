815a2cab032239c3163a1645984c70760399eeae
巫镇泉 <wuzhenquan1990@foxmail.com>
2017-01-16 08:17:10 +0800

    add redux-thunk(a kind of middleware)

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore,applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './containers/App'
 import todoApp from './reducers/reducers.js'
+import thunk from 'redux-thunk'
 
-let store = createStore(todoApp)
+let store = createStore(todoApp, applyMiddleware(thunk))
 // console.log(store.getState(),'store.getState()')
 // store 有 dispatch getState replaceReducer subscribe Symbol 这些函数
 let rootElement = document.getElementById('root')
